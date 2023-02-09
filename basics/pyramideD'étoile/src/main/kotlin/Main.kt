ecebfc2ec4f93d532bd86fea3e9b068ab5a4a206
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2023-02-09 10:57:46 +0100

    save comments

--- basics/pyramideD'étoile/src/main/kotlin/Main.kt
+++ basics/pyramideD'étoile/src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 // step by step to a pyramid of stars and lines of it defined by the user
-// 1. ask the user to enter a number
 fun main (){
+// 1. ask the user to enter a number
    println("Enter a number")
    val input: Int = readln().toInt()
-// 2. print a * for each number
+// 2. print a * for each number (to change it later to a pyramid of stars)
    for (input in 1..input){
       println("*")
    }
