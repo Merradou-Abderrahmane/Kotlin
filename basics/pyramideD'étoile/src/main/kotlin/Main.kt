7caad98c4e9c221d645a9fecf093398424fd97c2
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2023-02-09 15:28:08 +0100

    print a pyramid of stars

--- basics/pyramideD'étoile/src/main/kotlin/Main.kt
+++ basics/pyramideD'étoile/src/main/kotlin/Main.kt
@@ -3,13 +3,29 @@ fun main (){
 // 1. ask the user to enter a number
    println("Enter a number")
    val input: Int = readln().toInt()
+
 // 2. print a * for each number (to change it later to a pyramid of stars)
 //    for (input in 1..input){
 //       println("*")
 //    }
+
 // 3. print a triangle of stars
+   // for (i in 1..input){
+   //    for (j in 1..i){
+   //       print("*")
+   //    }
+   //    println()
+   // }
+   
+// 4. print a pyramid of stars
    for (i in 1..input){
+      for (j in 1..input-i){
+         print(" ")
+      }
       for (j in 1..i){
+         print("*")
+      }
+      for (j in 1..i-1){
          print("*")
       }
       println()
