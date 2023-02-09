590f75393a8f29b9be781df89f107d01350e691c
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2023-02-09 10:53:24 +0100

    get user input

--- basics/pyramideD'étoile/src/main/kotlin/Main.kt
+++ basics/pyramideD'étoile/src/main/kotlin/Main.kt
@@ -1,1 +1,6 @@
-// stars pyramid with kotlin , print * in a pyramid shape
+// step by step to a pyramid of stars and lines of it defined by the user
+// 1. ask the user to enter a number
+fun main (){
+   println("Enter a number")
+   val input: Int = readln().toInt()
+}
