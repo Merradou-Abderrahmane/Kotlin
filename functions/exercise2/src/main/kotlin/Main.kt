6cf9b7e608846d7f3ae986a339410607cc0efea4
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2023-02-10 10:27:00 +0100

    done products exercise

--- functions/exercise2/src/main/kotlin/Main.kt
+++ functions/exercise2/src/main/kotlin/Main.kt
@@ -1,8 +1,38 @@
-
-fun main (){
-    val names = mutableListOf<String>()
-    names.add("John")
-
-    println(names)
+val products = mutableListOf<String>()
+fun addProduct() {
+    print("Enter a product: ")
+    val product = readLine()!!.toString()
+    products.add(product)
 }
 
+fun showProducts() {
+    println("Products:")
+    for (product in products) {
+        println(product)
+    }
+}
+
+fun removeProduct() {
+    print("Enter a product: ")
+    val product = readLine()!!.toString()
+    products.remove(product)
+    // show the products after removing one
+    showProducts()
+}
+
+fun main() {
+    var option = 0
+    do {
+        println("1. Add product")
+        println("2. Remove product")
+        println("3. Show products")
+        println("4. Exit")
+        print("Enter an option: ")
+        option = readLine()!!.toInt()
+        when (option) {
+            1 -> addProduct()
+            2 -> removeProduct()
+            3 -> showProducts()
+        }
+    } while (option != 4)
+}
