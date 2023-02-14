f966092d309310f87474b357f050690894d5c054
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2023-02-14 09:27:07 +0100

    add init

--- classes/exercise3/src/main/kotlin/exercise3/myapp/Produit.kt
+++ classes/exercise3/src/main/kotlin/exercise3/myapp/Produit.kt
@@ -3,6 +3,9 @@ package exercise3.myapp
 class Produit (name: String, price: Int) {
     var name: String = name
     var price: Int = price
+    init {
+        println("hello")
+    }
 
     fun print(){
         print("product name is $name it's price is $price ")
