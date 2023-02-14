949867e01003dd897c20fc76c6c4607b093b2b17
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2023-02-14 11:02:46 +0100

    abstract class produit with name and price

--- classes/exercise3/src/main/kotlin/exercise3/myapp/Produit.kt
+++ classes/exercise3/src/main/kotlin/exercise3/myapp/Produit.kt
@@ -1,15 +1,4 @@
 package exercise3.myapp
 
-open class Produit (name: String, price: Int) {
-   open var name: String = name
-    var price: Int = price
-    open var test: string = "test"
-    init {
-        println("hello")
-    }
-
-    fun print(){
-
-        print("product name is $name it's price is $price ")
-    }
+abstract class Produit (val name: String, val price: Double) {
 }
